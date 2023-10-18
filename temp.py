7fab5d43bfa0665654cdd98f287503555e95d66a
Amber <amber.campbell@edu.sait.ca>
2023-10-17 20:27:13 -0600

    function celsius to fahrenheit

--- temp.py
+++ temp.py
@@ -1,3 +1,9 @@
+#celsius = 25
+#fahrenheit = celsius * 9/5 + 32
+#print (f"{celsius}celsius to fahrenheit is {fahrenheit}")
+
 celsius = 25
-fahrenheit = celsius * 9/5 + 32
-print (f"{celsius}celsius to fahrenheit is {fahrenheit}")
+def fahrenheit (celsius):
+    f = celsius * 9/5 + 32
+    return f
+print(fahrenheit(celsius))
