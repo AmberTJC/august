6565e472484cbe4cea6f1e8a99c12b3cb1a66e0b
Amber <amber.campbell@edu.sait.ca>
2023-10-17 20:34:43 -0600

    fahrenheit to celsius

--- temp.py
+++ temp.py
@@ -7,3 +7,9 @@ def fahrenheit (celsius):
     f = celsius * 9/5 + 32
     return f
 print(fahrenheit(celsius))
+
+fahren = 82
+def celci (fahren):
+    c = 5/9 * (fahren - 32)
+    return c
+print (celci(fahren))
